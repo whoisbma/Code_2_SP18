cd2784761456e52fb0ee854d86485d485e3758fa
Bryan Ma <hello@bryan.ma>
2018-02-09 13:30:10 -0500

    w3

--- week-03/w3_02_pong_with_collider/sketch.js
+++ week-03/w3_02_pong_with_collider/sketch.js
@@ -9,14 +9,20 @@
 
 var ball;
 var p1, p2;
+var p1Score = 0;
+var p2Score = 0;
 var p1Up = false;
 var p1Down = false;
 var p2Up = false;
 var p2Down = false;
 var margin = 20;
 var cnv;
-
+var paddleBounceSFX, hitColliderSFX;
 var collider;
+
+function preload() {
+
+}
 
 function centerCanvas() {
   var x = (windowWidth - width) / 2;
@@ -24,7 +30,13 @@ function centerCanvas() {
   cnv.position(x, y);
 }
 
+function windowResized() {
+  centerCanvas();
+}
+
 function setup() {
+  paddleBounceSFX = loadSound('assets/ballCollide.mp3', function() { console.log("loaded"); });
+  hitColliderSFX = loadSound('assets/hitCollider.mp3', function() { console.log("loaded"); });
   cnv = createCanvas(900, 500);
   centerCanvas();
   ball = new Ball();
@@ -36,41 +48,23 @@ function setup() {
 function draw() {
   background(0);
   drawField();
-  ball.update();
-  ball.display();
 
   p1.move(p1Up, p1Down);
   p2.move(p2Up, p2Down);
 
-
-
-
+  ball.update();
   p1.update();
   p2.update();
-  // collider.update();
-  checkCollision(ball, p1);
-  checkCollision(ball, p2);
-  // checkCollision(ball, collider);
+  collider.update();
+
   p1.display();
   p2.display();
-  // collider.display();
+  collider.display();
+  ball.display(); 
 
-}
-
-
-function checkCollision(ball, other) {
-
-  if (ball.pos.x + ball.width/2 > other.pos.x && 
-      ball.pos.x + ball.width/2 < other.pos.x + other.width && 
-      ball.pos.y + ball.height/2 > other.pos.y &&
-      ball.pos.y + ball.height/2 < other.pos.y + other.height) {
-    ball.collided(other);
-    other.collided(ball);
-  }
-}
-
-function windowResized() {
-  centerCanvas();
+  checkCollisionWithBall(ball, p1);
+  checkCollisionWithBall(ball, p2);
+  checkCollisionWithBall(ball, collider);
 }
 
 function drawField() {
@@ -82,6 +76,23 @@ function drawField() {
     var start = i;
     var finish = start + 15;
     line(width/2, start, width/2, finish);
+  }
+
+  fill(255);
+  noStroke();
+  textSize(64);
+  textAlign(CENTER, CENTER);
+  text(p1Score, width/2-50, 70);
+  text(p2Score, width/2+50, 70);
+}
+
+function checkCollisionWithBall(ball, other) {
+  if (ball.pos.x + ball.width/2 > other.pos.x && 
+      ball.pos.x + ball.width/2 < other.pos.x + other.width && 
+      ball.pos.y + ball.height/2 > other.pos.y &&
+      ball.pos.y + ball.height/2 < other.pos.y + other.height) {
+    ball.collided(other);
+    other.collided(ball);
   }
 }
 
@@ -97,8 +108,10 @@ function Ball() {
 
   this.update = function() {
     if (this.pos.x < -this.width) {
+      p2Score++;
       this.resetAfterPoint(0);
     } else if (this.pos.x > width) {
+      p1Score++;
       this.resetAfterPoint(1);
     }
 
@@ -122,20 +135,20 @@ function Ball() {
     this.vel = createVector(0, 0);
     this.speed = 7;
     if (whichPlayer === 1) {
-      this.resetAngle(4 * PI/6, 8 * PI/6);
+      this.getStartingAngle(4 * PI/6, 8 * PI/6);
     } else if (whichPlayer === 0) {
-      this.resetAngle(-PI/3, PI/3);
+      this.getStartingAngle(-PI/3, PI/3);
     }
   }
 
-  this.resetAngle = function(angleLow, angleHigh) {  
+  this.getStartingAngle = function(angleLow, angleHigh) {  
     var angle = random(angleLow, angleHigh);
     this.vel.x = cos(angle) * this.speed;
     this.vel.y = sin(angle) * this.speed;
   }
 
   this.collided = function(other) {
-    console.log('ball.collided()');
+    
   }
 };
 
@@ -180,30 +193,30 @@ function Paddle(num) {
   }
 
   this.collided = function(other) {
-    console.log('paddle collided');
-    var diff = other.pos.y - this.pos.y;
+    var diff = (other.pos.y + other.height/2) - this.pos.y;
     if (this.num === 0) {
       angle = map(diff, 0, this.height, -PI/3, PI/3);
     }
     if (this.num === 1) {
       angle = map(diff, this.height, 0, 4*PI/6, 8*PI/6);
     }
-    other.vel.set(cos(other.angle) * other.speed, sin(other.angle) * other.speed);
-    other.pos.add(other.vel);
+    other.speed += 1;
+    other.vel.x = cos(angle) * other.speed;
+    other.vel.y = sin(angle) * other.speed;
+    paddleBounceSFX.play();
   }
 }
 
 function Collider() {
-  this.pos = createVector(width/2, height/2);
-  this.speed = 5;
+  this.pos = createVector(random(200, width-200), random(300, height-300));
+  this.speed = 1;
   this.angle = 0;
-  this.vel = createVector(cos(this.angle) * this.speed, sin(this.angle) * this.speed);
-  this.width = 100;
-  this.height = 100;
+  this.vel = createVector(0, sin(this.angle) * this.speed);
+  this.width = 10;
+  this.height = 200;
 
   this.update = function() {
     this.angle = (this.angle + 0.05) % TWO_PI;
-    this.vel.x = cos(this.angle) * this.speed;
     this.vel.y = sin(this.angle) * this.speed;
     this.pos.add(this.vel);
   }
@@ -214,9 +227,23 @@ function Collider() {
   }
 
   this.collided = function(other) {
-    // this.width++;
-    // this.height++;
-    // other.speed *= 2;
+    if (other.speed > 1) {
+      other.speed -= 0.5;
+    }
+
+    other.angle = random(TWO_PI);
+    other.vel.x = cos(other.angle) * other.speed;
+    other.vel.y = sin(other.angle) * other.speed;
+
+    if (this.height > 0) {
+      this.pos.y += 20;
+      this.height -= 40;      
+    } else {
+      this.height = 0;
+    }
+    if (!hitColliderSFX.isPlaying()) {
+      hitColliderSFX.play();
+    }
   }
 }
 
