ca5fa7b0dc017304e54a824ae51777cf803ce30a
Bryan Ma <bryanhma@gmail.com>
2018-04-09 10:26:39 -0400

    apis

--- week-07/w7_01_APIs/sketch.js
+++ week-07/w7_01_APIs/sketch.js
@@ -8,7 +8,7 @@
 // giphy API
 // based on example by Dan Shiffman
 
-var giphySearch = 'https://api.giphy.com/v1/gifs/search?'
+var giphySearch = 'http://api.giphy.com/v1/gifs/search?'
 var query = 'q=doge'
 var apiKey = '&api_key=W6EoCb8070RoeMOEg3CTCdAPcugZv2Rc&limit=5'
 
