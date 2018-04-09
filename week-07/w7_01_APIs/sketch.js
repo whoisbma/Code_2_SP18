29690bc46b00545273d38cb0a072f465a7b25015
Bryan Ma <bryanhma@gmail.com>
2018-04-09 10:10:01 -0400

    apis

---
+++ week-07/w7_01_APIs/sketch.js
@@ -0,0 +1,28 @@
+// code 2
+// section a
+// bfa dt
+// spring 2018
+// bryan ma
+
+// week 7
+// giphy API
+// based on example by Dan Shiffman
+
+var giphySearch = 'http://api.giphy.com/v1/gifs/search?'
+var query = 'q=doge'
+var apiKey = '&api_key=W6EoCb8070RoeMOEg3CTCdAPcugZv2Rc&limit=5'
+
+function setup() {
+  noCanvas();
+  var url = giphySearch + query + apiKey;
+  loadJSON(url, gotData);
+}
+
+function gotData(data) {
+  console.log(data);
+
+  for (var i = 0; i < data.data.length; i++) {
+    createImg(data.data[i].images.original.url);    
+  }
+
+}
